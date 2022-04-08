94b24f36889d567ba2d894311878d09253d35f6c
Martín <magadiflo@gmail.com>
2022-04-08 12:26:02 -0500

    Recuperar Password HTML

--- src/app/components/usuario/usuario-routing.module.ts
+++ src/app/components/usuario/usuario-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent, }
+  { path: '', component: LoginComponent, },
+  { path: 'recuperar-password', component: RecuperarPasswordComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
