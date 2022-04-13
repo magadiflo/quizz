997ce571468e33022e4c22de8b5823d6432af32e
Martín <magadiflo@gmail.com>
2022-04-12 22:06:32 -0500

    Navbar

--- src/app/components/dashboard/navbar/navbar.component.ts
+++ src/app/components/dashboard/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.afAuth.signOut();
+    this.router.navigate(['/']);
   }
 
 }
