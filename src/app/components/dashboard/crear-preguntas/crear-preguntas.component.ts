07cf8a3cdf7ae3d7bcc7a2f4dfd25488a0154414
Martín <magadiflo@gmail.com>
2022-04-13 12:20:50 -0500

    Agregar Pregunta FormGroup

--- src/app/components/dashboard/crear-preguntas/crear-preguntas.component.ts
+++ src/app/components/dashboard/crear-preguntas/crear-preguntas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { QuizzService } from '../../../services/quizz.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-preguntas',
@@ -9,7 +10,32 @@ import { QuizzService } from '../../../services/quizz.service';
 })
 export class CrearPreguntasComponent implements OnInit {
 
-  constructor(private quizzService: QuizzService) { }
+  miFormulario: FormGroup = this.fb.group({
+    titulo: ['', [Validators.required]],
+    segundos: [10, [Validators.required]],
+    puntos: [100, [Validators.required]],
+    respuesta1: this.fb.group({
+      titulo: ['', [Validators.required]],
+      esCorrecto: [false, [Validators.required]],
+    }),
+    respuesta2: this.fb.group({
+      titulo: ['', [Validators.required]],
+      esCorrecto: [false, [Validators.required]],
+    }),
+    respuesta3: this.fb.group({
+      titulo: '',
+      esCorrecto: false,
+    }),
+    respuesta4: this.fb.group({
+      titulo: '',
+      esCorrecto: false,
+    }),
+  });
+
+
+  constructor(
+    private quizzService: QuizzService,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
     console.log(this.quizzService.tituloCuestionario, this.quizzService.descripcion);
