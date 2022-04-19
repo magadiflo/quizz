43bbe96153df689059d107fed3e851a3fdf0f10a
Martín <magadiflo@gmail.com>
2022-04-19 11:52:02 -0500

    Verificamos si el usuario está logueado

--- src/app/components/dashboard/list-cuestionarios/list-cuestionarios.component.ts
+++ src/app/components/dashboard/list-cuestionarios/list-cuestionarios.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-list-cuestionarios',
   templateUrl: './list-cuestionarios.component.html',
   styleUrls: ['./list-cuestionarios.component.css']
 })
-export class ListCuestionariosComponent implements OnInit {
+export class ListCuestionariosComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscriptionUser: Subscription = new Subscription();
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.subscriptionUser = this.afAuth.user.subscribe(user => {
+      if (user && user.emailVerified) {
+        //TODO: Cargar los cuestionarios
+        console.log(user);
+      } else {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionUser.unsubscribe();
   }
 
 }
