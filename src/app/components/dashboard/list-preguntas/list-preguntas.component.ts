b40f59a89192e3fad9bc8303fafea032bbb952e2
Martín <magadiflo@gmail.com>
2022-04-19 10:22:53 -0500

    Creamos objeto Cuestionario Parte 1

--- src/app/components/dashboard/list-preguntas/list-preguntas.component.ts
+++ src/app/components/dashboard/list-preguntas/list-preguntas.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { QuizzService } from '../../../services/quizz.service';
 import { Pregunta } from '../../../models/pregunta.model';
 import { Respuesta } from '../../../models/respuesta.model';
+import { Cuestionario } from '../../../models/cuestionario.model';
 
 @Component({
   selector: 'app-list-preguntas',
@@ -13,18 +15,36 @@ export class ListPreguntasComponent implements OnInit {
 
   listaPreguntas: Pregunta[] = [];
 
-  constructor(private quizzService: QuizzService) { }
+  constructor(
+    private quizzService: QuizzService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    if(this.quizzService.tituloCuestionario === '' || this.quizzService.descripcion === ''){
+      this.router.navigate(['/dashboard']);
+    }
     this.quizzService.getPreguntas()
       .subscribe(pregunta => {
         this.listaPreguntas.push(pregunta);
-        console.log(this.listaPreguntas);
       });
   }
 
   eliminarPregunta(index: number): void {
     this.listaPreguntas.splice(index, 1);
+  }
+
+  finalizarCuestionario(): void {
+    const cuestionario: Cuestionario = {
+      uid: '',
+      titulo: this.quizzService.tituloCuestionario,
+      descripcion: this.quizzService.descripcion,
+      codigo: '',
+      cantPreguntas: this.listaPreguntas.length,
+      fechaCreacion: new Date(),
+      listaPreguntas: this.listaPreguntas,
+      id: 1
+    }
+    console.log(cuestionario);  
   }
 
   activa(respuesta: Respuesta) {
