1111c26b9a9242f69f9382efd2aafb71a3c6d63b
Martín <magadiflo@gmail.com>
2022-04-20 12:06:24 -0500

    IngresarNombre Component Parte 2

--- src/app/components/jugar/ingresar-nombre/ingresar-nombre.component.ts
+++ src/app/components/jugar/ingresar-nombre/ingresar-nombre.component.ts
@@ -11,17 +11,39 @@ import { RespuestaQuizzService } from '../../../services/respuesta-quizz.service
 export class IngresarNombreComponent implements OnInit {
 
   nombre: string = '';
+  errorText: string = '';
+  error: boolean = false;
 
   constructor(
     private respuestaQuizzService: RespuestaQuizzService,
     private router: Router) { }
 
   ngOnInit(): void {
+    this.validarRefresh();
   }
 
   ingresarNombre() {
+    if(this.nombre.trim() === ''){
+      return this.errorMensaje('Ingrese su nombre');
+    }
     this.respuestaQuizzService.nombreParticipante = this.nombre;
     this.router.navigate(['/jugar', 'iniciar-contador']);
+  }
+
+  errorMensaje(texto: string): void {
+    this.errorText = texto;
+    this.error = true;
+
+    //* Mostramos el error por 4 segundos
+    setTimeout(() => {
+      this.error = false;
+    }, 4000);
+  }
+
+  validarRefresh() {
+    if(this.respuestaQuizzService.cuestionario == undefined){
+      this.router.navigate(['/']);
+    }
   }
 
 }
