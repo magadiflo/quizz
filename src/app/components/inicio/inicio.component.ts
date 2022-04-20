acab5eaabcd5ddb9a8aa1ab766b600892e7facd5
Martín <magadiflo@gmail.com>
2022-04-20 11:14:49 -0500

    Inicio Component - Validamos Código - Parte 2

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { RespuestaQuizzService } from '../../services/respuesta-quizz.service';
+import { Cuestionario } from '../../models/cuestionario.model';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit {
+export class InicioComponent implements OnInit, OnDestroy {
 
   error: boolean = false;
   pin: string = '';
   errorText: string = '';
   loading: boolean = false;
+  subscriptionCode: Subscription = new Subscription();
 
   constructor(private respuestaQuizzService: RespuestaQuizzService) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionCode.unsubscribe();
   }
 
   ingresar(): void {
@@ -26,12 +33,22 @@ export class InicioComponent implements OnInit {
     }
 
     this.loading = true;
-    this.respuestaQuizzService.searchByCode(this.pin)
+    this.subscriptionCode = this.respuestaQuizzService.searchByCode(this.pin)
       .subscribe({
         next: data => {
           this.loading = false;
           if (data.empty) {
             this.errorMensaje('PIN inválido');
+          } else {
+            data.forEach((element: any) => {
+              const cuestionario: Cuestionario = {
+                id: element.id,
+                ...element.data()
+              }
+              this.respuestaQuizzService.cuestionario = cuestionario;
+              // TODO: Redireccionar al próximo componente
+
+            });
           }
         },
         error: error => console.log(error)
--- src/app/services/respuesta-quizz.service.ts
+++ src/app/services/respuesta-quizz.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { Cuestionario } from '../models/cuestionario.model';
 
 
 @Injectable({
@@ -8,9 +9,13 @@ import { Observable } from 'rxjs';
 })
 export class RespuestaQuizzService {
 
+  cuestionario!: Cuestionario;
+
   constructor(private _firestore: AngularFirestore) { }
 
   searchByCode(code: string): Observable<any> {
     return this._firestore.collection('cuestionarios', ref => ref.where('codigo', '==', code)).get();
   }
+
+
 }
