f31812242177489a03a602778133265ec2db5b8b
Martín <magadiflo@gmail.com>
2022-04-25 13:06:12 -0500

    Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { InicioComponent } from './components/inicio/inicio.component';
 import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: InicioComponent, },
-  { 
-    path: 'usuario', 
-    loadChildren: () => import('./components/usuario/usuario.module').then(m => m.UsuarioModule), 
+  {
+    path: 'usuario',
+    loadChildren: () => import('./components/usuario/usuario.module').then(m => m.UsuarioModule),
   },
-  { 
+  {
     path: 'jugar',
-    loadChildren: () => import('./components/jugar/jugar.module').then(m => m.JugarModule), 
+    loadChildren: () => import('./components/jugar/jugar.module').then(m => m.JugarModule),
   },
-  { 
-    path: 'dashboard', 
-    component: DashboardComponent, 
-    loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule), 
+  {
+    path: 'dashboard',
+    canActivate: [AuthGuard],
+    canLoad: [AuthGuard],
+    component: DashboardComponent,
+    loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule),
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree, Router } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanLoad {
+
+  constructor(private router: Router) { }
+
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const user = localStorage.getItem('user');
+    if (user === null) {
+      this.router.navigate(['/']);
+    }
+    return true;
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const user = localStorage.getItem('user');
+    if (user === null) {
+      this.router.navigate(['/']);
+    }
+    return true;
+  }
+}
